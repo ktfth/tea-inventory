d8b1cf9eef08310c6c26433d972bed2293e617a6
ktfth <44123854+ktfth@users.noreply.github.com>
2023-03-22 00:56:22 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  const initialHerbs = ['Mate', 'Preto', 'Camomila', 'Maracujá'];
+  let [herbs, setHerbs] = React.useState(initialHerbs);
+  let [name, setName] = React.useState('');
+
+  function searchHerb(e) {
+    setName(e.target.value);
+    setHerbs(herbs.filter((herb) => herb.includes(name)));
+
+    if (!herbs.some((herb) => herb.includes(e.target.value))) {
+      setHerbs(initialHerbs);
+    }
+
+    if (e.target.value === '') {
+      setHerbs(initialHerbs);
+    }
+  }
+
+  function cleanSearch(e) {
+    e.preventDefault();
+    setName('');
+    setHerbs(initialHerbs);
+  }
+
+  return (
+    <div>
+      <h1>Inventário de ervas para chá</h1>
+      <input
+        className="tea-search"
+        type="text"
+        placeholder="Erva"
+        onChange={(e) => searchHerb(e)}
+        value={name}
+      />
+      <span>
+        {' '}
+        <a href="#" onClick={(e) => cleanSearch(e)}>
+          Limpar busca
+        </a>
+      </span>
+      <p>{name ? 'Você esta procurando por: ' + name : ''}</p>
+      <ul>
+        {herbs.map((herb) => (
+          <li>{herb}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# tea-inventory
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-7a3q7s)
